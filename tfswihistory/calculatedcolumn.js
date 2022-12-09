80b37f0eadbfe80fe6ace0c2f69c6745cfcefb4a
Alexey Totmakov <Totmakov@avp.ru>
2022-12-09 21:59:21 +0300

    continue working on calculated values in query result
    add OnDomChange reaction

--- tfswihistory/calculatedcolumn.js
+++ tfswihistory/calculatedcolumn.js
@@ -17,20 +17,74 @@ function change_latest_column(items) {
         let row = document.getElementById(items[i][0])
         let placeholder = row.childNodes[row.childElementCount - 1];
         placeholder.innerText = 'waiting for value...';
-        get_wi_history_new(get_tfs_url(), wi_id, (val) => { placeholder.innerText = concatenate_history(val, 'System.AssignedTo') });
+        row.addEventListener('change', (event) => { 
+            console.log(event)
+        });
+        row.oncopy = (event) => { 
+            console.log('copy', event)
+        }
+        row.onbeforecopy = (event) => { 
+            console.log('beforecopy', event)
+        }
+        row.onselect = (event) => { 
+            console.log('select', event)
+        }
+        placeholder.onclick = (event) => { 
+            console.log('click', event)
+        } 
+
+
+        get_wi_history_new(get_tfs_url(), wi_id, (val) => { placeholder.innerText = concatenate_history(val, 'KL.TRDate') });
     }
 }
 
 function concatenate_history(fields_history, fieldname) {
     let result = '';
     for (var v in fields_history[fieldname]) { 
-        result += fields_history[fieldname][v].val + ' => '; 
+        result += formatDateTime(fields_history[fieldname][v].val) + ' => '; 
     }
     return result;
 }
 
 window.document.onkeydown = (event) => {
-    if (event.key == 'k') {
+    const targetNode = document.getElementById('vss_4');
+
+    // Options for the observer (which mutations to observe)
+    const config = { childList: true, subtree: true };
+    
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+        console.log(`The ${mutationList.length} mutation occurs.`);
+        for (const mutation of mutationList) {
+        if (mutation.type === 'childList') {
+            if (mutation.addedNodes.length >0 ) {
+                console.log('A child node has been added.');
+                mutation.addedNodes.forEach( (n) => {
+                    if (n.childNodes.length > 0) {
+                    let wi_id = n.childNodes[0].innerText;
+                    n.childNodes[7].innerText = 'loading...';
+                    get_wi_history_new(get_tfs_url(), wi_id, (val) => { n.childNodes[7].innerText = concatenate_history(val, 'KL.TRDate') });
+                    }
+                });
+            }
+            if (mutation.removedNodes.length >0 ) {
+                console.log('A child node has been removed.');
+            }
+        } else if (mutation.type === 'attributes') {
+          console.log(`The ${mutation.attributeName} attribute was modified.`);
+        } else {
+            console.log(`The ${mutation} was modified.`);
+        }
+      }
+    };
+    
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+    if (event.key == 'h') {
         let header, items = get_wis_table()
         change_latest_column(items);
     }
